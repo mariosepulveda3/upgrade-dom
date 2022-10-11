e8686cf683ee74a1b2b079cfd07f2179c4c2f42b
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-11 20:26:04 +0200

    DONE, iteracion1

--- iteración-1.js
+++ iteración-1.js
@@ -1,14 +1,35 @@
-// 1.1 Usa querySelector para mostrar por consola el botón con la clase .showme
+// 1.1 Usa querySelector para mostrar por consola el botón con 
+// la clase .showme
 
-// 1.2 Usa querySelector para mostrar por consola el h1 con el id #pillado
+const showButton = document.querySelector(".showme")
+console.log(showButton)
+
+// 1.2 Usa querySelector para mostrar por consola el h1 con el 
+// id #pillado
+
+const showH1 = document.querySelector('#pillado');
+console.log(showH1);
 
 // 1.3 Usa querySelector para mostrar por consola todos los p
 
-// 1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
+const showAllP = document.querySelectorAll('p');
+console.log(showAllP);
 
-// 1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo 
+// 1.4 Usa querySelector para mostrar por consola todos los 
+// elementos con la clase.pokemon
+
+const classPokemon = document.querySelectorAll('.pokemon');
+console.log(classPokemon);
+
+// 1.5 Usa querySelector para mostrar por consola todos los 
+// elementos con el atributo 
 // data-function="testMe".
 
-// 1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo 
-// data-function="testMe".
+const allTestMe = document.querySelectorAll('[data-function="testMe"]');
+console.log(allTestMe);
 
+// 1.6 Usa querySelector para mostrar por consola el 3 
+// personaje con el atributo data-function="testMe".
+
+const testMeNumber3 = document.querySelector('[data-function="testMe"]');
+console.log(testMeNumber3[2]);
